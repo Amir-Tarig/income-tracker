e97eb42ddc6f47a59c5f946f3c99a2847964ae35
Miro <MiroPhoenix91@gmail.com>
2021-04-20 18:37:36 +0200

    income tracker

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <Header :totalIncome="state.totalIncome"/>
   <Form  @Handle-Data="handleData"/>
+  <Income-list :state='state' />
 </template>
 
 <script>
 import Header from './components/header.vue'
 import Form from './components/Form.vue'
 import {reactive , computed } from 'vue'
+import IncomeList from './components/IncomeList.vue'
 export default {
   name: 'App',
-  components: {Header , Form},
+  components: {Header , Form, IncomeList},
     setup() {
       const state = reactive({
         income: [],
@@ -25,13 +27,14 @@ export default {
       function handleData (data) {
         let d = data.date.split('-')
         let newD = new Date(d[0], d[1], d[2])
-        
+
          state.income = [...state.income , {
            id: Date.now(),
            desc: data.desc,
            value: parseInt(data.value),
-           date: newD.getTime
+           date: newD.getTime()
          }]
+         console.log(state.income)
       }
       return {
         handleData,
@@ -42,5 +45,13 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  background: #eee;
+}
 </style>
---
+++ src/components/IncomeList.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="income-list">
+      <Items v-for="data in state.income" :key="data.id" :income="data"/>
+ </div>
+</template>
+
+<script>
+import Items from './Items.vue'
+export default {
+    components: {Items},
+    props: {
+        state: Object
+    }
+}
+</script>
+
+<style scoped>
+    .income-list {
+        margin-top: 60px;
+        padding: 15px;
+    }
+</style>
